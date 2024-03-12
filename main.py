434c08b32ce8145ade7e9174fe82b5c27ca12b8b
murali <muralimano86@gmail.com>
2024-03-12 21:22:31 +0000

    Noun is class, verb is method. Create hotel and ticket class. Add methods.

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas
+
+
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == hotel_id, "name"].squeeze()
+
+    def book(self):
+        """ Book the hotel """
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """ Check the availability of the hotel """
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for reservation.
+        Here is your booking confirmation
+        Name: {self.name} 
+        Hotel: {self.hotel.name}
+        """
+
+        return content
+
+
+df = pandas.read_csv("hotels.csv", dtype={"id":str})
+print(df)
+hotel_id = input("Enter the hotel id: ")
+hotel = Hotel(hotel_id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free")
